630e8a9a73d783c115599a184e713349968d870e
Vincent <vleclerc@inow.fr>
2021-11-29 12:16:19 +0100

    add logging

--- Identity1121/IdentityServer/Program.cs
+++ Identity1121/IdentityServer/Program.cs
@@ -1,9 +1,12 @@
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,16 +14,67 @@ namespace IdentityServer
 {
     public class Program
     {
-        public static void Main(string[] args)
+        private static string AppName = typeof(Program).Namespace;
+        public static int Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var configuration = GetConfiguration();
+            Log.Logger = CreateSerilogLogger(configuration);
+            //CreateHostBuilder(args).Build().Run();
+            try
+            {
+                Log.Information("Configuring web host", AppName);
+                var host = BuildWebHost(configuration, args);
+
+                Log.Information("Starting web host...", AppName);
+                host.Run();
+                return 0;
+
+            }catch(Exception e)
+            {
+                Log.Fatal(e, "Program crash", AppName);
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        private static IWebHost BuildWebHost(IConfiguration configuration, string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .CaptureStartupErrors(false)
+                .UseStartup<Startup>()
+                .UseConfiguration(configuration)
+                .UseSerilog()
+                .Build();
+
+        private static Serilog.ILogger CreateSerilogLogger(IConfiguration configuration)
+        {
+            var logstahUrl = configuration["Serilog:LogstashUrl"];
+            var cfg = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .Enrich.WithProperty("ApplicationContext", AppName)
+                .Enrich.FromLogContext()
+                .WriteTo.Console();
+
+            //cfg.WriteTo.Http(logstahUrl);
+            return cfg.CreateLogger();
+        }
+
+        private static IConfiguration GetConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
+            return builder.Build();
+        }
+
+        /*public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                });*/
     }
 }
